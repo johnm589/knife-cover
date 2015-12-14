85a2cd9b80c9606002e1432d211c112babb14a33
John <johnm589@yahoo.com>
2015-12-13 18:29:17 -0800

    fixed it

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
   if request.patch?
     if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
       redirect_to checkout_payment_path
+    end
   end
 end
 # Payment Controller
@@ -26,4 +27,6 @@ end
         current_order.confirm!
         session[:order_id] = nil
         redirect_to root_path, :notice => "Order has been placed successfully!"
+end
+end
 end
